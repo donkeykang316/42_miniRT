0110555115fb53d01453b2594309338c4dff87f7
donkeykang316 <kang_an_an@hotmail.de>
2024-07-31 21:12:32 +0200

    tbc

--- sphere_test/sphere.c
+++ sphere_test/sphere.c
@@ -6,6 +6,7 @@
 # include <stdlib.h>
 # include <stdbool.h>
 # include <stdint.h>
+# include <string.h>
 
 typedef struct  s_vector
 {
@@ -44,6 +45,67 @@ typedef struct s_camera
     t_vector    pixel00_loc;
     t_vector    camera_center;
 } t_camera;
+
+void    ft_putstr_fd(char *s, int fd)
+{
+    write(fd, s, strlen(s));
+}
+
+void    ft_putdouble_fd(double n, int fd)
+{
+    char    c;
+    int     precision = 6;
+
+    // Handle negative numbers
+    if (n < 0)
+    {
+        write(fd, "-", 1);
+        n = -n;
+    }
+
+    // Extract and print integer part
+    long long int_part = (long long)n;
+    double fraction_part = n - int_part;
+
+    if (int_part == 0)
+        write(fd, "0", 1);
+    else
+    {
+        // Recursive function to print each digit of integer part
+        if (int_part >= 10)
+            ft_putdouble_fd(int_part / 10, fd); // Use precision 0 here for recursive calls
+        c = (int_part % 10) + '0';
+        write(fd, &c, 1);
+    }
+
+    // If precision is zero, no need to print decimal point
+    if (precision == 0)
+        return;
+
+    // Print decimal point
+    write(fd, ".", 1);
+
+    // Print fractional part up to specified precision
+    while (precision-- > 0)
+    {
+        fraction_part *= 10;
+        int digit = (int)fraction_part;
+        c = digit + '0';
+        write(fd, &c, 1);
+        fraction_part -= digit;
+    }
+}
+
+void    print_vector(t_vector v, int fd)
+{
+    ft_putstr_fd("x: ", fd);
+    ft_putdouble_fd(v.x, fd);
+    ft_putstr_fd("\ny: ", fd);
+    ft_putdouble_fd(v.y, fd);
+    ft_putstr_fd("\nz: ", fd);
+    ft_putdouble_fd(v.z, fd);
+    ft_putstr_fd("\n", fd);
+}
 
 t_vector	vec_init(double x, double y, double z)
 {
@@ -203,26 +265,26 @@ void    write_color(t_vector pixel_color)
 
 }
 
-void    camera_init(t_camera camera)
+void    camera_init(t_camera *camera)
 {
     t_vector    vec;
 
-    camera.aspect_ratio = 1;
-    camera.image_width = 400;
-    camera.image_height = (int)(camera.image_width / camera.aspect_ratio);
-    if (camera.image_height < 1)
-        camera.image_height = 1;
-    camera.focal_length = 1.0;
-    camera.viewport_height = 2.0;
-    camera.viewport_width = camera.viewport_height * (double)(camera.image_width / camera.image_height);
-    camera.camera_center = vec_init(0, 0, 0);
-    vec = vec_init(0, 0, camera.focal_length);
-    camera.viewport_u = vec_init(camera.viewport_width, 0, 0);
-    camera.viewport_v = vec_init(0, -(camera.viewport_height), 0);
-    camera.pixel_delta_u = divi_vec_int(camera.viewport_u, camera.image_width);
-    camera.pixel_delta_v = divi_vec_int(camera.viewport_v, camera.image_height);
-    camera.viewport_upper_left = subtrac_vec_vec(camera.camera_center, add_vec_vec(vec, add_vec_vec(divi_vec_int(camera.viewport_u, 2), divi_vec_int(camera.viewport_v, 2))));
-    camera.pixel00_loc = add_vec_vec(camera.viewport_upper_left, multi_vec_doub(add_vec_vec(camera.pixel_delta_u, camera.pixel_delta_v), 0.5));
+    camera->aspect_ratio = 1;
+    camera->image_width = 400;
+    camera->image_height = (int)(camera->image_width / camera->aspect_ratio);
+    if (camera->image_height < 1)
+        camera->image_height = 1;
+    camera->focal_length = 1.0;
+    camera->viewport_height = 2.0;
+    camera->viewport_width = camera->viewport_height * (double)(camera->image_width / camera->image_height);
+    camera->camera_center = vec_init(0, 0, 0);
+    vec = vec_init(0, 0, camera->focal_length);
+    camera->viewport_u = vec_init(camera->viewport_width, 0, 0);
+    camera->viewport_v = vec_init(0, -(camera->viewport_height), 0);
+    camera->pixel_delta_u = divi_vec_int(camera->viewport_u, camera->image_width);
+    camera->pixel_delta_v = divi_vec_int(camera->viewport_v, camera->image_height);
+    camera->viewport_upper_left = subtrac_vec_vec(camera->camera_center, add_vec_vec(vec, add_vec_vec(divi_vec_int(camera->viewport_u, 2), divi_vec_int(camera->viewport_v, 2))));
+    camera->pixel00_loc = add_vec_vec(camera->viewport_upper_left, multi_vec_doub(add_vec_vec(camera->pixel_delta_u, camera->pixel_delta_v), 0.5));
 }
 
 bool    hit_sphere(t_vector center, double radius, t_ray ray)
@@ -265,7 +327,29 @@ void    render()
 
     i = 0;
     j = 0;
-    camera_init(camera);
+    camera_init(&camera);
+    ft_putstr_fd("aspect_ratio: ", 2);
+    ft_putdouble_fd(camera.aspect_ratio, 2);
+    ft_putstr_fd("\nimage_width: ", 2);
+    ft_putdouble_fd(camera.image_width, 2);
+    ft_putstr_fd("\nimage_height: ", 2);
+    ft_putdouble_fd(camera.image_height, 2);
+    ft_putstr_fd("\nfocal_length: ", 2);
+    ft_putdouble_fd(camera.focal_length, 2);
+    ft_putstr_fd("\ncamera_center: ", 2);
+    print_vector(camera.camera_center, 2);
+    ft_putstr_fd("\nviewport_height: ", 2);
+    ft_putdouble_fd(camera.viewport_height, 2);
+    ft_putstr_fd("\npixel00_loc: ", 2);
+    print_vector(camera.pixel00_loc, 2);
+    ft_putstr_fd("\nviewport_u: ", 2);
+    print_vector(camera.viewport_u, 2);
+    ft_putstr_fd("\npixel_delta_u: ", 2);
+    print_vector(camera.pixel_delta_u, 2);
+    ft_putstr_fd("\nviewport_v: ", 2);
+    print_vector(camera.pixel_delta_v, 2);
+    ft_putstr_fd("\nviewport_upper_left: ", 2);
+    print_vector(camera.viewport_upper_left, 2);
     printf("P3\n%d %d\n255\n", (int)camera.image_width, (int)camera.image_height);
     while (j < camera.image_height)
     {
